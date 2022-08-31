c31114e0fef1dd3fd806de5263313305ea8c96df
gfpkelly1986 <gfpkelly@gmail.com>
2022-08-31 18:06:49 +0000

    Add get repo info function to github.html

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -19,6 +19,28 @@ function userInformationHTML(user) {
 };
 
 
+function repoInformationHTML() {
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+
+    let listItemsHTML = repos.map((repo) => {
+        return `<li>
+                    <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                </li>`
+    })
+
+    return `<div class="clearfix repo-list">
+                <p>
+                    <strong>Repo List:</strong>
+                </p>
+                <ul>
+                    ${listItemsHTML.join("\n")}
+                </ul>
+            </div>`
+}
+
+
 /* Using jquery 'when' and 'then' to create a promise*/ 
 function fetchGitHubInformation(event) {
     const username = $('#gh-username').val();
@@ -33,12 +55,15 @@ function fetchGitHubInformation(event) {
         </div>`);
         // using .getJSON to query the github API.
         $.when(
-            $.getJSON(`https://api.github.com/users/${username}`)
+            $.getJSON(`https://api.github.com/users/${username}`),
+            $.getJSON(`https://api.github.com/users/${username}/repos`)
         ).then(
             //anonomous function that takes the response as a parameter
-            function (response){
-                const userData = response;
+            function (response, secondResponse){
+                const userData = response[0];
+                const repoData = secondResponse[0];
                 $("#gh-user-data").html(userInformationHTML(userData));
+                $("#gh-repo-data").html(repoInformationHTML(repoData));
             }, function(errorResponse){
                 if (errorResponse.status === 404) {
                     $('#gh-user-data').html(`<h2>No info found for user ${username}</h2>`);
